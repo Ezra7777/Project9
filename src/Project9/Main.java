bb522c439ce7c680ca25afb1a16349367c57a511
EsraOzturk <esosh77@hotmail.com>
2020-01-07 12:52:17 -0500

    Project9

--- src/Project9/Main.java
+++ src/Project9/Main.java
@@ -1,18 +1,18 @@
 package Project9;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-import java.lang.reflect.Parameter;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -44,31 +44,42 @@ public class Main {
         // to select "budget groups ", the forth child of the first child of  the first child of group items
         driver.findElement(By.cssSelector(".group-items > :nth-child(6) > .children > :nth-child(1) > .children > :nth-child(1)")).click();
         driver.findElement(By.cssSelector("[data-icon='plus']")).click();
-        // driver.findElement(By.xpath("//*[@class='mat-mini-fab mat-button-base mat-accent']")).click();
+        String descriptionname = "Mygroup";
 
-        driver.findElement(By.xpath("//*[@id=\"mat-input-5\"]")).sendKeys("Smyrcvlk");
-        // driver.findElement(By.xpath("mat-option-10")).click();
+        driver.findElement(By.xpath("//*[@id=\"mat-input-5\"]")).sendKeys(descriptionname);
+
         driver.findElement(By.cssSelector("#mat-select-2")).click();
         driver.findElement(By.xpath("//*[@id=\"mat-option-11\"]/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"mat-input-7\"]")).sendKeys("01/06/2020");
-        driver.findElement(By.xpath("//*[@id=\"mat-input-8\"]")).sendKeys("03/06/2020");
-        driver.findElement(By.xpath("//*[@id=\"mat-input-6\"]")).sendKeys("comment");
+
+        String startdate = "01/01/2020";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-7\"]")).sendKeys(startdate);
+
+        String enddate ="01/07/2020";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-8\"]")).sendKeys(enddate);
+
+        String comment ="Success!";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-6\"]")).sendKeys(comment);
         WebElement saveIcon = driver.findElement(By.cssSelector("mat-dialog-container [data-icon=\"save\"]"));
         saveIcon.click();
         try {
-            wait.until( ExpectedConditions.visibilityOfElementLocated( By.cssSelector( "[aria-label='Budget Group successfully created']" ) ) );
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[aria-label='Budget Group successfully created']")));
 
-        } catch( Exception e) {
-           Assert.fail("Delete failure, e");
+        } catch (Exception e) {
+            Assert.fail("Delete failure, e");
         }
+        String text = driver.findElement(By.xpath("//td[text()='Mygroup']")).getText();
+        Assert.assertEquals(descriptionname, text);
 
+        String text2 = driver.findElement(By.xpath("//td[text()='01/01/2020']")).getText();
+        Assert.assertEquals(startdate,text2);
+
+        String text3 = driver.findElement(By.xpath("//td[text()='01/07/2020']")).getText();
+        Assert.assertEquals(enddate,text3);
 
     }
+
     @AfterClass
-    public void quit(){
+    public void quit() {
         driver.quit();
     }
 }
-
-
-
--- src/Project9/MyProject9.java
+++ src/Project9/MyProject9.java
@@ -11,53 +11,79 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class MyProject9 {
-    @Test
-    public void test(){
-   //
+
+    public static void main(String[] args) {
+
+
+        //
         System.setProperty("webdriver.chrome.driver", "/Users/EsraOzturk/Downloads/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("https:////test-basqar.mersys.io/");
         driver.manage().window().maximize();
 
-        driver.findElement( By.cssSelector( "[formcontrolname=\"username\"]" ) ).sendKeys( "admin" );
-        driver.findElement( By.cssSelector( "[formcontrolname=\"password\"]" ) ).sendKeys( "admin" );
-        driver.findElement( By.cssSelector( "button[aria-label=\"LOGIN\"]" ) ).click();
+        driver.findElement(By.cssSelector("[formcontrolname=\"username\"]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("[formcontrolname=\"password\"]")).sendKeys("admin");
+        driver.findElement(By.cssSelector("button[aria-label=\"LOGIN\"]")).click();
 
-        WebDriverWait wait = new WebDriverWait( driver, 5 );
-        driver.manage().timeouts().implicitlyWait( 3, TimeUnit.SECONDS );
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 
         // to select "Setup", the first child of group items
-        driver.findElement( By.cssSelector( ".group-items > :nth-child(6)" ) ).click();
+        driver.findElement(By.cssSelector(".group-items > :nth-child(6)")).click();
         // to select "Parameters", the first child of  the first child of group items
-        driver.findElement( By.cssSelector( ".group-items > :nth-child(6) > .children > :nth-child(1)" ) ).click();
+        driver.findElement(By.cssSelector(".group-items > :nth-child(6) > .children > :nth-child(1)")).click();
         // to select "Nationalities", the forth child of the first child of  the first child of group items
-        driver.findElement( By.cssSelector( ".group-items > :nth-child(6) > .children > :nth-child(1) > .children > :nth-child(1)" ) ).click();
+        driver.findElement(By.cssSelector(".group-items > :nth-child(6) > .children > :nth-child(1) > .children > :nth-child(1)")).click();
         driver.findElement(By.cssSelector("[data-icon='plus']")).click();
         // driver.findElement(By.xpath("//*[@class='mat-mini-fab mat-button-base mat-accent']")).click();
 
-        driver.findElement(By.xpath("//*[@id=\"mat-input-5\"]")).sendKeys("Mygroupp");
+        String descriptionname = "Mygroup";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-5\"]")).sendKeys(descriptionname);
+
+
         driver.findElement(By.cssSelector("#mat-select-2")).click();
         driver.findElement(By.xpath("//*[@id=\"mat-option-11\"]/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"mat-input-7\"]")).sendKeys("01/06/2020");
-        driver.findElement(By.xpath("//*[@id=\"mat-input-8\"]")).sendKeys("01/07/2020");
-        driver.findElement(By.xpath("//*[@id=\"mat-input-6\"]")).sendKeys("Comment");
+
+        String startdate = "01/01/2020";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-7\"]")).sendKeys(startdate);
+
+        String enddate ="01/07/2020";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-8\"]")).sendKeys(enddate);
+
+        String comment ="Success!";
+        driver.findElement(By.xpath("//*[@id=\"mat-input-6\"]")).sendKeys(comment);
+
         driver.findElement(By.xpath("//div[@class='mat-tab-label mat-ripple ng-star-inserted']")).click();
         driver.findElement(By.xpath("//input[@class='mat-input-element mat-form-field-autofill-control cdk-text-field-autofill-monitored ng-untouched ng-pristine ng-invalid']")).sendKeys("3");
 
-        driver.findElement(By.xpath("//ms-decimal-field//input")).sendKeys("22115");
+        driver.findElement(By.xpath("//ms-decimal-field//input")).sendKeys("221122");
         driver.findElement(By.xpath("//button[@class='mat-raised-button mat-button-base mat-basic ng-star-inserted']")).click();
-        WebElement saveIcon = driver.findElement( By.cssSelector( "mat-dialog-container [data-icon=\"save\"]" ) );
+        WebElement saveIcon = driver.findElement(By.cssSelector("mat-dialog-container [data-icon=\"save\"]"));
         saveIcon.click();
         try {
-            wait.until( ExpectedConditions.visibilityOfElementLocated( By.cssSelector( "[aria-label='Budget Group successfully created']" ) ) );
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[aria-label='Budget Group successfully created']")));
             System.out.println("Creation success!");
-        } catch( Exception e) {
+        } catch (Exception e) {
             System.out.println("Creation failure!");
         }
 
 
+        String text = driver.findElement(By.xpath("//td[text()='Mygroup']")).getText();
+        if (descriptionname.equals(text)) {
+            System.out.println("succes");
+        }
+        String text2 = driver.findElement(By.xpath("//td[text()='01/01/2020']")).getText();
+        if (startdate.equals(text2)) {
+            System.out.println("succes");
+        }
+        String text3 = driver.findElement(By.xpath("//td[text()='01/07/2020']")).getText();
+        if (enddate.equals(text3)) {
+            System.out.println("succes");
+        }
 
+
+        System.out.println(text);
 
     }
 }
